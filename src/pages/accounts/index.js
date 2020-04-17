dda29cb0093fb111ab7d848bf8ec03e1982cd8bd
Aatish <aatish.bondge@objectedge.com>
2020-04-17 18:49:17 +0530

    account and job selection part done

--- src/pages/accounts/index.js
+++ src/pages/accounts/index.js
@@ -1,13 +1,28 @@
 import { AccountCardListing } from "../../components/accounts";
 import React from "react";
+import { useHistory } from 'react-router-dom';
+import { connect } from "react-redux";
 import PageWrapper from "../../components/shared/pageWrapper";
 
-export const Accounts = () => {
-  return (
-    <PageWrapper>
-      <AccountCardListing />
-    </PageWrapper>
-  );
-};
+function mapStateToProps({ authentication }) {
+  return { 
+    logged: authentication.logged
+  };
+}
+
+export const Accounts = connect(mapStateToProps)(({ logged }) => {
+  const history = useHistory();
+  if(logged){
+    return (
+      <PageWrapper>
+        <AccountCardListing />
+      </PageWrapper>
+    );
+  }
+  else{
+    history.push("/");
+    return null
+  }
+});
 
 export default Accounts;
