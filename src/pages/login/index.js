1e1b4c36c16429b9f0404eb7cdb676217957f9f4
Aatish <aatish.bondge@objectedge.com>
2020-04-18 11:52:24 +0530

    background added on login page

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -16,14 +16,20 @@ const useStyles = makeStyles((theme) => ({
     width: "100vw",
     height: "100vh",
   },
+  background: {
+    backgroundImage: "url('https://wmu.epdemos.com/static/front-banner-660b66b6af4d34222e0e98b67357d485.jpg')",
+    width: "100%",
+    backgroundRepeat  : 'no-repeat',
+    backgroundPosition: 'center',
+  }
 }));
 
 export const Login = connect(mapStateToProps)(({ logged = false, user }) => {
   const classes = useStyles();
   return (
-    <Container className={classes.root}>
-      <LoginForm />
-    </Container>
+    <div className={classes.root}>
+      <div className={classes.background}><LoginForm /></div>
+    </div>
   );
 });
 
