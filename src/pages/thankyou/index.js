056c547d712a689b8ca5642a7222d16f379104c0
Bhushan <bhushan.patil@objectedge.com>
2020-04-19 16:40:11 +0530

    Order id display done

--- src/pages/thankyou/index.js
+++ src/pages/thankyou/index.js
@@ -2,25 +2,29 @@ import React from "react";
 import { useHistory } from 'react-router-dom';
 import { connect } from "react-redux";
 import PageWrapper from "../../components/shared/pageWrapper";
+import queryString from 'query-string'
 
 function mapStateToProps({ authentication }) {
   return { 
     logged: authentication.logged
   };
 }
-export const ThankYou = connect(mapStateToProps)(({ logged }) => {
+export const ThankYou = connect(mapStateToProps)(({ logged, location }) => {
   const history = useHistory();
-  if(logged){
+  const values = queryString.parse(location.search);
+  const order_id = values.order_id;
+  //if(logged){
     return (
       <PageWrapper>
-        Thank you! Your Order has been placed successfully. Details will be sent through email to you!
+        {`Thank you! Your Order has been placed successfully. Details will be sent through email to you!
+    Your order id is "${order_id}"`}
       </PageWrapper>
     );
-  }
-  else{
-    history.push("/");
-    return null
-  }
+  // }
+  // else{
+  //   history.push("/");
+  //   return null
+  // }
 });
 
 export default ThankYou;
