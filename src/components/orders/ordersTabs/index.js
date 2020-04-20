76e3a0a6f3128f9bfa73d1039f1ace2342a5f2d5
Aatish <aatish.bondge@objectedge.com>
2020-04-20 15:36:52 +0530

    tabs issue resolved

--- src/components/orders/ordersTabs/index.js
+++ src/components/orders/ordersTabs/index.js
@@ -3,9 +3,6 @@ import { Box, Tab, Tabs, Typography } from "@material-ui/core";
 import React, { useState,useEffect  } from "react";
 import { BrowserRouter as Router, NavLink, Route, useHistory, Switch } from "react-router-dom";
 import { connect } from "react-redux";
-import { createBrowserHistory } from "history";
-import { OrdersList } from '../ordersList';
-import { PlaceOrder } from '../placeOrder';
 import sagaTypes from "../../../sagas/sagaTypes";
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -16,8 +13,6 @@ const useStyles = makeStyles((theme) => ({
     backgroundColor: theme.palette.background.paper,
   },
 }));
-
-// const BASENAME = "/orders";
 
 const TabPanel = (props) => {
   const { children, value, index, ...other } = props;
@@ -35,89 +30,6 @@ const TabPanel = (props) => {
     </Typography>
   );
 };
-
-// const a11yProps = (index) => {
-//   return {
-//     id: `wrapped-tab-${index}`,
-//     "aria-controls": `wrapped-tabpanel-${index}`,
-//   };
-// };
-
-// const getTabItems = (role) => {
-//   return ((typeof role === "string" && role) || "").toString().toLowerCase() ===
-//     "approver"
-//     ? APPROVER_ITEMS
-//     : CUSTOMER_ITEMS;
-// };
-
-// const mapStateToProps = ({ authentication }) => ({ authentication });
-
-// export const OrdersTabs = connect(mapStateToProps)(
-//   ({ authentication = {} }) => {
-//     const { userDetails = {} } = authentication || {};
-//     const { data = {} } = userDetails || {};
-//     const { type = "customer" } = data || {};
-//     const currentTabItems = getTabItems(type) || [];
-//     const firstTab = [...currentTabItems].shift();
-//     const { tabValue } = firstTab || {};
-//     const [value, setValue] = useState(tabValue);
-
-
-//     const history = useHistory();
-//     //const history = createBrowserHistory({ basename: '/orders' });
-
-
-//     const renderTabs = () => {
-//       return currentTabItems.map((tabItem, index) => {
-//         const { tabLabel, tabValue } = tabItem;
-
-//         return (
-//           <Tab
-//             component={NavLink}
-//             key={`tab-${index}`}
-//             value={tabValue}
-//             label={tabLabel}
-//             to={`/orders/${tabValue}`}
-//             wrapped
-//             {...a11yProps(tabValue)}
-//           />
-//         );
-//       });
-//     };
-
-//     const renderTabsPanels = () => {
-//       return currentTabItems.map((tabItem, index) => {
-//         const { tabComponent, tabValue } = tabItem;
-
-//         return (
-//           <TabPanel key={index} value={value} index={tabValue}>
-//             <Route path={`/orders/${tabValue}`} exact>
-//               {tabComponent()}
-//             </Route>
-//           </TabPanel>
-//         );
-//       });
-//     };
-
-//     return (
-//       <Switch>
-//         <Tabs
-//           value={props.history.location.pathname}
-//           indicatorColor="primary"
-//           textColor="primary"
-//           onChange={handleChange}
-//           aria-label="wrapped label tabs example"
-//           variant="fullWidth"
-//         >
-//           {renderTabs()}
-//         </Tabs>
-//         {renderTabsPanels()}
-//         </Switch>
-//     );
-//   }
-// );
-
-   
 
 const a11yProps = (index) => {
   return {
@@ -142,10 +54,15 @@ const a11yProps = (index) => {
     }
   }
   const getTabItems = (role) => {
+    if(role){
       return ((typeof role === "string" && role) || "").toString().toLowerCase() ===
         "approver"
         ? APPROVER_ITEMS
         : CUSTOMER_ITEMS;
+    }
+    else{
+      return null
+    }
     };
   
 export const OrdersTabs =  connect(mapStateToProps,mapDispatchToProps)(({authentication = {}, jobs = {}, getRoleOfCustomer, role, resetRoleOfCustomer}) => {
@@ -175,8 +92,8 @@ export const OrdersTabs =  connect(mapStateToProps,mapDispatchToProps)(({authent
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
-    console.log("changed",newValue)
   };
+
       return (
         <Route
         path="/"
@@ -189,26 +106,17 @@ export const OrdersTabs =  connect(mapStateToProps,mapDispatchToProps)(({authent
           onChange={handleChange}
           aria-label="wrapped label tabs example"
           variant="fullWidth"
-        >
-         
-          
+        > 
           {tabs && tabs.map((t,i)=>{
             return <Tab label={t.tabLabel} value={`${t.tabValue}`} component={NavLink} to={`/orders/${t.tabValue}`} {...a11yProps(t.tabValue)}/>
-          })}         
-          
-          
+          })}
         </Tabs>
         {tabs && tabs.map((t,i)=>{
-          return (<TabPanel key={i} value={value} index={t.tabValue}>
-                  
-                  <Route path={`/orders/${t.tabValue}`} render={() => t.tabComponent()} exact />
-                  
-            </TabPanel>)
+          return (
+          <TabPanel key={i} value={value} index={t.tabValue}>
+            {t.tabComponent()}
+          </TabPanel>)
         })}
-        
-                  
-        
-       
         </div>
         )}
         />
