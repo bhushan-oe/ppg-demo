58c52fb10c36a4f35643af13a82e6c152a3bc059
Deeparti wani <deeparti.wani@objectedge.com>
2020-04-21 18:37:53 +0530

    Opened modal to show order details on click of order id

--- src/components/orders/orderDetails/index.js
+++ src/components/orders/orderDetails/index.js
@@ -1,21 +1,19 @@
 import {
   Grid,
-  ExpansionPanel,
-  ExpansionPanelSummary,
-  ExpansionPanelDetails,
-  makeStyles,
-  Paper,
+  makeStyles
 } from "@material-ui/core";
 import { connect } from "react-redux";
 import OrderApproveButton from "../orderApproveButton";
 import { ORDER_STATUS_APPROVAL_PENDING } from "../ordersStatus";
 import React from "react";
 import OrderSkuDetails from './OrderSkuDetails';
+import Modal from '@material-ui/core/Modal';
 
 const useStyles = makeStyles((theme) => ({
   root: {
     'align-items': 'center',
-    color: 'black'
+    color: 'black',
+    borderBottom: '2px solid grey',
   },
   expansionSummaryWrapper: {
     padding: '0 !important',
@@ -24,12 +22,6 @@ const useStyles = makeStyles((theme) => ({
     fontSize: theme.typography.pxToRem(15),
     fontWeight: theme.typography.fontWeightRegular,
     padding: '15px'
-  },
-  paper: {
-    width: "100%",
-    padding: "10px 0",
-    "margin-bottom": "20px",
-    "box-shadow": "none !important"
   },
   horizontalBar: {
     border: '0.5px solid rgb(223, 219, 219)',
@@ -43,8 +35,28 @@ const useStyles = makeStyles((theme) => ({
   },
   expansionDetails: {
     padding: '0 30px 0 0 !important',
-  }
+  },
+  paper: {
+    position: 'absolute',
+    width: '40%',
+    backgroundColor: theme.palette.background.paper,
+    border: '2px solid #000',
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
 }));
+
+function getModalStyle() {
+  const top = 50;
+  const left = 50;
+
+  return {
+    top: `${top}%`,
+    left: `${left}%`,
+    transform: `translate(-${top}%, -${left}%)`,
+  };
+}
+
 
 const mapStateToProps = ({ orders, role }) => ({ orders, role });
 
@@ -66,51 +78,65 @@ export const OrderDetails = connect(
     }
   } = order;
 
+  const [modalStyle] = React.useState(getModalStyle);
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-       
-    <ExpansionPanel className={classes.expansionPanelWrapper}>
-      <ExpansionPanelSummary
-        aria-controls={`panel-${index}-content`}
-        // expandIcon={<ExpandMore />}
-        id={`panel-${index}-header`}
-        className={classes.expansionSummaryWrapper}
-      >
-      {displayApproveButton && order ? (
-        <Grid className={classes.root} container>
-          <Grid className={classes.heading} item xs={2}>{id}</Grid>
-          <Grid className={classes.heading} item xs={2}>{name}</Grid>
-          <Grid className={classes.heading} item xs={2}>{created_at.split("T",1)}</Grid>
-          <Grid className={classes.heading} item xs={2}>{totalPrice}</Grid>
-          <Grid className={classes.heading} item xs={2}>
-            <OrderApproveButton 
-              show={displayApproveButton} 
-              orderId={id}
-              title={"Approve"} />
-          </Grid>
-          <Grid className={classes.heading} item xs={2}>
-            <OrderApproveButton 
-              show={displayApproveButton} 
-              orderId={id}
-              title={"Reject"} />
-          </Grid>
+    displayApproveButton && order ? (
+      <Grid className={classes.root} container>
+        <Grid className={classes.heading} item xs={2} onClick={handleOpen}>{id}</Grid>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="simple-modal-title"
+          aria-describedby="simple-modal-description"
+        >
+          <div style={modalStyle} className={classes.paper}>
+            <OrderSkuDetails order={order.included.items} />
+          </div>
+        </Modal>
+        <Grid className={classes.heading} item xs={2}>{name}</Grid>
+        <Grid className={classes.heading} item xs={2}>{created_at.split("T",1)}</Grid>
+        <Grid className={classes.heading} item xs={2}>{totalPrice}</Grid>
+        <Grid className={classes.heading} item xs={2}>
+          <OrderApproveButton 
+            show={displayApproveButton} 
+            orderId={id}
+            title={"Approve"} />
         </Grid>
-        ) : (
-          order && <Grid className={classes.root} container>
-            <Grid className={classes.heading} item xs={3}>{id}</Grid>
-            <Grid className={classes.heading} item xs={3}>{name}</Grid>
-            <Grid className={classes.heading} item xs={3}>{created_at.split("T",1)}</Grid>
-            <Grid className={classes.heading} item xs={3}>{totalPrice}</Grid>
-          </Grid>
-        )
-      }
-      </ExpansionPanelSummary>
-      <hr className={classes.horizontalBar}/>
-      <ExpansionPanelDetails className={classes.expansionDetails}>
-        <Paper className={classes.paper}>
-          <OrderSkuDetails order={order.included.items}/>
-        </Paper>
-      </ExpansionPanelDetails>
-    </ExpansionPanel>
+        <Grid className={classes.heading} item xs={2}>
+          <OrderApproveButton 
+            show={displayApproveButton} 
+            orderId={id}
+            title={"Reject"} />
+        </Grid>
+      </Grid>
+      ) : (
+        order && <Grid className={classes.root} container>
+          <Grid className={classes.heading} item xs={3} onClick={handleOpen}>{id}</Grid>
+          <Modal
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="simple-modal-title"
+            aria-describedby="simple-modal-description"
+          >
+            <div style={modalStyle} className={classes.paper}>
+              <OrderSkuDetails order={order.included.items} />
+            </div>
+          </Modal>
+          <Grid className={classes.heading} item xs={3}>{name}</Grid>
+          <Grid className={classes.heading} item xs={3}>{created_at.split("T",1)}</Grid>
+          <Grid className={classes.heading} item xs={3}>{totalPrice}</Grid>
+        </Grid>
+      )
   );
 });
 
